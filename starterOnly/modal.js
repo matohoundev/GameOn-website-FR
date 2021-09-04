a6e757218dee6962c874f4399ccf97ee5d2e6c48
matohoundev <matohoundev@gmail.com>
2021-09-04 16:25:37 +0200

    #1 fix close Modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -9,15 +9,22 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const btnModalClose = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//close modal event
+btnModalClose.addEventListener('click', closeModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
